bd2b13b55247c035483bf4cd77aa9bd44894c8c9
Ravish-chauhan <ravish.chauhan2006@gmail.com>
2025-09-25 16:04:52 +0530

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-// ✅ MongoDB Connection
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
